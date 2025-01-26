9f71ba6b7c1aca14c064b6b06b0857b6552a0f08
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-01-27 00:23:39 +0100

    ok

--- src/gdfx2.py
+++ src/gdfx2.py
@@ -10,18 +10,28 @@ from alive_progress import alive_bar
 
 # MOMENTUM?
 # BiLSTM: Wie mit den beiden Richtungen umgehen?
+# DropOut?
 
 parser = argparse.ArgumentParser(prog = "gdfx2", description = "generalizable deepfake detection framework")
 parser.add_argument("mode", choices = ["train", "test"], help = "mode of operation")
+
+# dataset
 parser.add_argument("-sl", "--sequence-length", type = int, default = 16, help = "sequence length (8 to 32 for DF40), default is 16")
-parser.add_argument("-lr", "--learning-rate", type = float, default = 0.1, help = "learning rate (0 to 1), default is 0.1")
-parser.add_argument("-ep", "--epochs", type = int, default = 3, help = "training epochs, default is 3")
-parser.add_argument("-bs", "--batch-size", type = int, default = 16, help = "samples per batch, default is 16")
-parser.add_argument("-hs", "--hidden-size", type = int, default = 69, help = "internal lstm features, default is 69")
-parser.add_argument("-nl", "--num-layers", type = int, default = 1, help = "stacked lstm layers, default is 1")
+
+# training
+parser.add_argument("-bs", "--batch-size", type = int, default = 8, help = "samples per batch, default is 8")
+parser.add_argument("-ep", "--epochs", type = int, default = 1, help = "training epochs, default is 1")
+parser.add_argument("-lr", "--learning-rate", type = float, default = 0.01, help = "learning rate (0 to 1), default is 0.01")
+
+# model
+parser.add_argument("-hs", "--hidden-size", type = int, default = 256, help = "internal lstm features (net width), default is 256")
+parser.add_argument("-nl", "--num-layers", type = int, default = 3, help = "stacked lstm layers (net depth), default is 3")
+
+# io actions
 parser.add_argument("-sm", "--save-model", type = str, default = None, help = "save model after training to given path")
 parser.add_argument("-lm", "--load-model", type = str, default = None, help = "load model prior training/for testing from given path")
 parser.add_argument("-f", "--force", action = "store_true", help = "force overwriting when using -sm")
+
 args = parser.parse_args()
 
 MODE_TRAIN: bool = args.mode == "train"
@@ -29,37 +39,37 @@ MODE_TEST: bool = args.mode == "test"
 if MODE_TRAIN == MODE_TEST:
     raise AssertionError("rip")
 
-if args.sequencelength < 8 or args.sequencelength > 32:
+if args.sequence_length < 8 or args.sequence_length > 32:
     raise AssertionError("sequence length out of range")
-if args.learningrate <= 0 or args.learningrate >= 1:
-    raise AssertionError("learning rate out of range")
+if args.batch_size < 1:
+    raise AssertionError("batch size out of range")
 if args.epochs < 1:
     raise AssertionError("epoch size out of range")
-if args.batchsize < 1:
-    raise AssertionError("batch size out of range")
-if args.hiddensize < 1:
+if args.learning_rate <= 0 or args.learning_rate >= 1:
+    raise AssertionError("learning rate out of range")
+if args.hidden_size < 1:
     raise AssertionError("hidden size out of range")
-if args.numlayers < 1:
+if args.num_layers < 1:
     raise AssertionError("num layers out of range")
 
 SAVE_MODEL = None
-if args.savemodel:
-    SAVE_MODEL: Path = Path(args.savemodel).resolve()
+if args.save_model:
+    SAVE_MODEL: Path = Path(args.save_model).resolve()
     if SAVE_MODEL.exists() and not args.force:
         raise FileExistsError(f"file '{SAVE_MODEL}' does already exist, use -f to override")
 LOAD_MODEL = None
-if args.loadmodel:
-    LOAD_MODEL: Path = Path(args.loadmodel).resolve()
+if args.load_model:
+    LOAD_MODEL: Path = Path(args.load_model).resolve()
     if not LOAD_MODEL.exists():
         raise FileNotFoundError(f"file '{LOAD_MODEL} does not exist'")
 
 INPUT_SIZE = 3 * 256 * 256 # total amount of input features per timestamp is equal to size of image tensor
-SEQUENCE_LENGTH = args.sequencelength
-LEARNING_RATE = args.learningrate
+SEQUENCE_LENGTH = args.sequence_length
+BATCH_SIZE = args.batch_size
 EPOCHS = args.epochs
-BATCH_SIZE = args.batchsize
-HIDDEN_SIZE = args.hiddensize
-NUM_LAYERS = args.numlayers
+LEARNING_RATE = args.learning_rate
+HIDDEN_SIZE = args.hidden_size
+NUM_LAYERS = args.num_layers
 CLASSES = {
     "face_reenact": 0,
     "face_swap": 1
@@ -72,16 +82,25 @@ DF40_TRANSFORMATION = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor()
 ])
 
-sys.exit(0)
-
 try:
     if MODE_TRAIN:
+        print("(>) parameters")
+        print(f"\tinput size: {INPUT_SIZE}")
+        print(f"\tsequence length: {SEQUENCE_LENGTH}")
+        print(f"\tbatch size: {BATCH_SIZE}")
+        print(f"\tepochs: {EPOCHS}")
+        print(f"\tlearning rate: {LEARNING_RATE}")
+        print(f"\thidden size: {HIDDEN_SIZE}")
+        print(f"\tnum layers: {NUM_LAYERS}")
+        print(f"\tclasses: {len(CLASSES.keys())}")
         print("(>) initializing dataset")
         dataset_train = DF40ImageSequenceDataset("/home/gdfx2/io", train = True, sequence_length = SEQUENCE_LENGTH, transform = DF40_TRANSFORMATION)
 
         print("(>) setting up dataloader")
         dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size = BATCH_SIZE, shuffle = True)
         batches: int = len(dataloader_train) - 1 # crash fix - 1
+        print(f"\tbatches per epoch: {batches}")
+        print(f"\ttotal batches: {EPOCHS * batches}")
 
         bilstm: BiLSTM = BiLSTM(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, len(CLASSES.keys()))
         bilstm = bilstm.cuda()
@@ -92,15 +111,12 @@ try:
             # TODO: load optimizer state_dict
 
         lossf = torch.nn.CrossEntropyLoss()
-        optim = torch.optim.SGD(bilstm.parameters(), lr = LEARNING_RATE)
+        optim = torch.optim.AdamW(bilstm.parameters(), lr = LEARNING_RATE)
         print(f"(>) model: {bilstm}")
 
         with alive_bar(EPOCHS * batches, title=f"BiLSTM {EPOCHS}*{batches}@{BATCH_SIZE}") as pbar:
-            loss_running = 0.
-            loss_last = 0.
-
             for e in range(EPOCHS):
-                print(f"epoch {e + 1}")
+                print(f"(>) === epoch {e + 1} ===")
                 loss_epoch = 0.
 
                 bilstm.train(True)
@@ -129,10 +145,11 @@ try:
 
                     # sum up error
                     loss_epoch += loss.item()
+                    #print(f"(>) batch {i + 1} loss: {loss.item()}")
                     pbar(1)
                 loss_epoch = loss_epoch / batches
                 bilstm.eval()
-                print(f"  >>> {loss_epoch}")
+                print(f"(>) epoch loss avg: {loss_epoch}")
         if SAVE_MODEL:
             print(f"(>) saving model to '{SAVE_MODEL}'")
             torch.save(bilstm.state_dict(), SAVE_MODEL)
