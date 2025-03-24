c38e7c3fea772bd1802bc7879807340ae2339003
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-03-24 18:46:28 +0100

    q

--- src/gdfx2.py
+++ src/gdfx2.py
@@ -60,8 +60,8 @@ test_sequences, test_labels = list(test_sequences), list(test_labels)
 print("Preprocessing items...")
 train_dataset = tf.data.Dataset.from_tensor_slices((train_sequences, train_labels))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_sequences, test_labels))
-train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True)
-test_dataset = test_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(test_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True)
+train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True).batch(BATCH_SIZE)
+test_dataset = test_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(test_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True).batch(BATCH_SIZE)
 
 print("Prefetching items...")
 train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
@@ -122,7 +122,7 @@ if pl.Path(IO_PATH + "/model.weights.h5").exists():
 
 print("############################## TRAINING ##############################")
 
-history = model.fit(train_dataset, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=test_dataset, validation_freq=3, callbacks=[model_checkpoint, lr_scheduler, early_stopping])
+history = model.fit(train_dataset, epochs=EPOCHS, validation_data=test_dataset, validation_freq=3, callbacks=[model_checkpoint, lr_scheduler, early_stopping])
 
 print("############################## STORING/CONVERT ##############################")
 print(f"Saving latest model state to '{IO_PATH + '/model_final.pb'}'")
