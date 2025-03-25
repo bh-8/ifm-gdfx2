949db2159f1be387fded8ef7ce8d101bd8170d4a
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-03-25 19:11:26 +0100

    m

--- src/gdfx2.py
+++ src/gdfx2.py
@@ -19,7 +19,71 @@ WEIGHT_DECAY      = 3e-3
 DROPOUT           = 3e-1
 FEATURE_EXTRACTOR = "resnet"
 
-# TODO: test optimieren (shuffle raus etc.)
+print("############################## DATASET ##############################")
+
+def df40_list_labeled_items(split_path: pl.Path):
+    list_sequences: list[list[str]] = []
+    list_labels: list[int] = []
+    for class_id, class_name in enumerate(CLASS_LIST):
+        class_path: pl.Path = pl.Path(split_path) / class_name
+        if not class_path.exists():
+            print(f"E {class_path} does not exists!")
+            continue
+        # gather and sort frames, truncate too long sequences
+        for i in sorted([e for e in class_path.glob("**/") if e.match("frames/*")]):
+            sequential_paths: list[pl.Path] = sorted([f for f in i.glob("*")], key = lambda x : int(x.stem))[:SEQ_LEN]
+            if len(sequential_paths) >= SEQ_LEN: # minimum sequence length requirement
+                list_sequences.append([str(x) for x in sequential_paths])
+                list_labels.append(class_id)
+
+    return list_sequences, list_labels
+
+def df40_load_and_preprocess(path_sequence: list[str], label: int):
+    def _load_image(image_path: str):
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_png(image, channels=3)
+        image = tf.image.resize(image, [256, 256])
+        #image = image / 255.0
+        return image
+    return tf.stack([tf.keras.applications.resnet.preprocess_input(_load_image(elem)) for elem in tf.unstack(path_sequence)] if FEATURE_EXTRACTOR == "resnet" else [_load_image(elem) for elem in tf.unstack(path_sequence)]), tf.one_hot(label, len(CLASS_LIST))
+
+print("Enumerating items...")
+train_sequences, train_labels = df40_list_labeled_items(pl.Path(IO_PATH + "/df40/train").resolve())
+test_sequences, test_labels = df40_list_labeled_items(pl.Path(IO_PATH + "/df40/test").resolve())
+
+train_data = list(zip(train_sequences, train_labels))
+test_data = list(zip(test_sequences, test_labels))
+random.shuffle(train_data)
+random.shuffle(test_data)
+train_sequences, train_labels = zip(*train_data)
+test_sequences, test_labels = zip(*test_data)
+train_sequences, train_labels = list(train_sequences), list(train_labels)
+test_sequences, test_labels = list(test_sequences), list(test_labels)
+
+print("Preprocessing items...")
+train_dataset = tf.data.Dataset.from_tensor_slices((train_sequences, train_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_sequences, test_labels))
+train_dataset_classes = collections.Counter([int(l.numpy()) for (_, l) in train_dataset])
+test_dataset_classes = collections.Counter([int(l.numpy()) for (_, l) in test_dataset])
+
+print("Train Dataset:")
+for i, c in enumerate(CLASS_LIST):
+    print(f"  {c}: {train_dataset_classes[i]}x")
+
+class_weights = {i: len(train_dataset) / (c * len(train_dataset_classes)) for i, c in train_dataset_classes.items()}
+print("Class Weights:")
+for i, c in enumerate(CLASS_LIST):
+    print(f"  {c}: {class_weights[i]}")
+
+print("Test Dataset:")
+for i, c in enumerate(CLASS_LIST):
+    print(f"  {c}: {test_dataset_classes[i]}x")
+
+print("Prefetching items...")
+train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True).batch(BATCH_SIZE)
+test_dataset = test_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).batch(BATCH_SIZE)
+train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+test_dataset = test_dataset.prefetch(tf.data.AUTOTUNE)
 
 print("############################## MODEL ##############################")
 
@@ -86,72 +150,6 @@ with strategy.scope():
                 print(f"Epoch {epoch + 1}: Freezed all layers of baseline model, set learning rate to {new_lr}.")
 
 model.summary()
-
-print("############################## DATASET ##############################")
-
-def df40_list_labeled_items(split_path: pl.Path):
-    list_sequences: list[list[str]] = []
-    list_labels: list[int] = []
-    for class_id, class_name in enumerate(CLASS_LIST):
-        class_path: pl.Path = pl.Path(split_path) / class_name
-        if not class_path.exists():
-            print(f"E {class_path} does not exists!")
-            continue
-        # gather and sort frames, truncate too long sequences
-        for i in sorted([e for e in class_path.glob("**/") if e.match("frames/*")]):
-            sequential_paths: list[pl.Path] = sorted([f for f in i.glob("*")], key = lambda x : int(x.stem))[:SEQ_LEN]
-            if len(sequential_paths) >= SEQ_LEN: # minimum sequence length requirement
-                list_sequences.append([str(x) for x in sequential_paths])
-                list_labels.append(class_id)
-
-    return list_sequences, list_labels
-
-def df40_load_and_preprocess(path_sequence: list[str], label: int):
-    def _load_image(image_path: str):
-        image = tf.io.read_file(image_path)
-        image = tf.image.decode_png(image, channels=3)
-        image = tf.image.resize(image, [256, 256])
-        #image = image / 255.0
-        return image
-    return tf.stack([tf.keras.applications.resnet.preprocess_input(_load_image(elem)) for elem in tf.unstack(path_sequence)] if FEATURE_EXTRACTOR == "resnet" else [_load_image(elem) for elem in tf.unstack(path_sequence)]), tf.one_hot(label, len(CLASS_LIST))
-
-print("Enumerating items...")
-train_sequences, train_labels = df40_list_labeled_items(pl.Path(IO_PATH + "/df40/train").resolve())
-test_sequences, test_labels = df40_list_labeled_items(pl.Path(IO_PATH + "/df40/test").resolve())
-
-train_data = list(zip(train_sequences, train_labels))
-test_data = list(zip(test_sequences, test_labels))
-random.shuffle(train_data)
-random.shuffle(test_data)
-train_sequences, train_labels = zip(*train_data)
-test_sequences, test_labels = zip(*test_data)
-train_sequences, train_labels = list(train_sequences), list(train_labels)
-test_sequences, test_labels = list(test_sequences), list(test_labels)
-
-print("Preprocessing items...")
-train_dataset = tf.data.Dataset.from_tensor_slices((train_sequences, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_sequences, test_labels))
-train_dataset_classes = collections.Counter([int(l.numpy()) for (_, l) in train_dataset])
-test_dataset_classes = collections.Counter([int(l.numpy()) for (_, l) in test_dataset])
-
-print("Train Dataset:")
-for i, c in enumerate(CLASS_LIST):
-    print(f"  {c}: {train_dataset_classes[i]}x")
-
-class_weights = {i: len(train_dataset) / (c * len(train_dataset_classes)) for i, c in train_dataset_classes.items()}
-print("Class Weights:")
-for i, c in enumerate(CLASS_LIST):
-    print(f"  {c}: {class_weights[i]}")
-
-print("Test Dataset:")
-for i, c in enumerate(CLASS_LIST):
-    print(f"  {c}: {test_dataset_classes[i]}x")
-
-print("Prefetching items...")
-train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.025), reshuffle_each_iteration=True).batch(BATCH_SIZE)
-test_dataset = test_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).batch(BATCH_SIZE)
-train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
-test_dataset = test_dataset.prefetch(tf.data.AUTOTUNE)
 
 print("############################## TRAINING ##############################")
 
