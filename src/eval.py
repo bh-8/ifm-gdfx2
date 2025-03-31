ac1a5115197baeba538208b8a7830c94748365a8
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-03-31 21:33:04 +0200

    m

--- src/eval.py
+++ src/eval.py
@@ -13,9 +13,9 @@ import time
 CLASS_LIST        = ["original", "face_swap", "face_reenact"]
 IO_PATH           = "./io"
 IMG_SIZE          = (224, 224, 3)
-SEQ_LEN           = 8
-BATCH_SIZE        = 12
-FEATURE_EXTRACTOR = "efficientnet" # "resnet" # "efficientnet"
+SEQ_LEN           = 12
+BATCH_SIZE        = 8
+FEATURE_EXTRACTOR = "resnet" # "resnet" # "efficientnet"
 
 print("############################## DATASET ##############################")
 
