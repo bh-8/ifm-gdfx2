c6a4e65d3f26b75f07bdcced3273a04eda254f63
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-03-31 21:22:39 +0200

    m

--- src/eval.py
+++ src/eval.py
@@ -15,7 +15,7 @@ IO_PATH           = "./io"
 IMG_SIZE          = (224, 224, 3)
 SEQ_LEN           = 8
 BATCH_SIZE        = 12
-FEATURE_EXTRACTOR = "resnet" # "efficientnet"
+FEATURE_EXTRACTOR = "efficientnet" # "resnet" # "efficientnet"
 
 print("############################## DATASET ##############################")
 
