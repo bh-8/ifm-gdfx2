69d24015a724b6105812b96ca1e5f4551b71b28a
Bernhard Birnbaum <bernhard.birnbaum@ovgu.de>
2025-03-31 04:08:40 +0200

    m

--- src/gdfx2.py
+++ src/gdfx2.py
@@ -84,7 +84,7 @@ for i, c in enumerate(CLASS_LIST):
     print(f"  {c}: {test_dataset_classes[i]}x")
 
 print("Prefetching items...")
-train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.02), reshuffle_each_iteration=True).batch(BATCH_SIZE)
+train_dataset = train_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(int(float(train_dataset.cardinality()) * 0.01), reshuffle_each_iteration=True).batch(BATCH_SIZE)
 test_dataset = test_dataset.map(df40_load_and_preprocess, num_parallel_calls=tf.data.AUTOTUNE).batch(BATCH_SIZE)
 train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
 test_dataset = test_dataset.prefetch(tf.data.AUTOTUNE)
